b3311fa3961b15b2336f3690558379537ca4d6ce
Eunice Beltran <eunicesausbeltran@yahoo.com>
2018-07-19 15:22:12 +0800

    Add Post, Comment and User models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'comments/index'
+  get 'comments/new'
+  get 'comments/show'
+  get 'comments/destroy'
+
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/destroy'
+
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/destroy'
+
 end
